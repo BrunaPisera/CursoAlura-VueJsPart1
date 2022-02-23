d353b6fa827d201d1798a9f97db35b3a1a93811e
Bruna Pisera <pissara2@gmail.com>
2022-02-23 10:44:41 -0300

    Initial commit

---
+++ alurapic/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="corpo">
+    <meu-menu :rotas="routes" />
+    <transition name="pagina">
+      <router-view> </router-view>
+      <!--Serve para indicar para o template da página principal da aplicação o local no qual os demais componentes carregados através de rotas devem ser renderizados.-->
+    </transition>
+  </div>
+</template>
+
+<script>
+import { routes } from "./routes";
+import Menu from "./components/shared/menu/Menu.vue";
+
+export default {
+  components: {
+    "meu-menu": Menu
+  },
+
+  data() {
+    return {
+      routes
+    };
+  }
+};
+</script>
+
+<style>
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.3s;
+}
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
+}
+</style>
